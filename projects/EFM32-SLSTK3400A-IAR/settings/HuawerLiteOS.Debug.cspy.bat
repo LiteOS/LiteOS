2a00a70f7bda56b3a023069042338ba7ae21c223
lssacpeng <pengzhouhu@huawei.com>
2018-01-11 10:19:56 +0800

    merge projects files form liuxuming

--- projects/EFM32-SLSTK3400A-IAR/settings/HuawerLiteOS.Debug.cspy.bat
+++ projects/EFM32-SLSTK3400A-IAR/settings/HuawerLiteOS.Debug.cspy.bat
@@ -25,7 +25,7 @@ if not "%~1" == "" goto debugFile
 
 @echo on 
 
-"C:\Program Files (x86)\IAR Systems\Embedded Workbench 8.0\common\bin\cspybat" -f "C:\Users\Administrator\Desktop\porting\LiteOS_Kernel\projects\EFM32-SLSTK3400A_IAR\settings\HuawerLiteOS.Debug.general.xcl" --backend -f "C:\Users\Administrator\Desktop\porting\LiteOS_Kernel\projects\EFM32-SLSTK3400A_IAR\settings\HuawerLiteOS.Debug.driver.xcl" 
+"D:\Program Files (x86)\IAR Systems\Embedded Workbench 8.0\common\bin\cspybat" -f "D:\git\xuming\LiteOS_Kernel_12_1\projects\EFM32-SLSTK3400A-IAR\settings\HuawerLiteOS.Debug.general.xcl" --backend -f "D:\git\xuming\LiteOS_Kernel_12_1\projects\EFM32-SLSTK3400A-IAR\settings\HuawerLiteOS.Debug.driver.xcl" 
 
 @echo off 
 goto end 
@@ -34,7 +34,7 @@ goto end
 
 @echo on 
 
-"C:\Program Files (x86)\IAR Systems\Embedded Workbench 8.0\common\bin\cspybat" -f "C:\Users\Administrator\Desktop\porting\LiteOS_Kernel\projects\EFM32-SLSTK3400A_IAR\settings\HuawerLiteOS.Debug.general.xcl" "--debug_file=%~1" --backend -f "C:\Users\Administrator\Desktop\porting\LiteOS_Kernel\projects\EFM32-SLSTK3400A_IAR\settings\HuawerLiteOS.Debug.driver.xcl" 
+"D:\Program Files (x86)\IAR Systems\Embedded Workbench 8.0\common\bin\cspybat" -f "D:\git\xuming\LiteOS_Kernel_12_1\projects\EFM32-SLSTK3400A-IAR\settings\HuawerLiteOS.Debug.general.xcl" "--debug_file=%~1" --backend -f "D:\git\xuming\LiteOS_Kernel_12_1\projects\EFM32-SLSTK3400A-IAR\settings\HuawerLiteOS.Debug.driver.xcl" 
 
 @echo off 
 :end
