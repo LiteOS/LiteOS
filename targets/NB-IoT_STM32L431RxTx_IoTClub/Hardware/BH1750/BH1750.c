b815fd0b339b4c8589221224fa406f57887f2aba
Supowang08 <35022334+Supowang08@users.noreply.github.com>
2018-05-22 10:02:11 +0800

    modify code style
    modify some code style

--- targets/NB-IoT_STM32L431RxTx_IoTClub/Hardware/BH1750/BH1750.c
+++ targets/NB-IoT_STM32L431RxTx_IoTClub/Hardware/BH1750/BH1750.c
@@ -28,18 +28,18 @@ uint16_t result=0;
   */
 static void I2C_InitGPIO(void)
 {
-  GPIO_InitTypeDef GPIO_InitStruct;
-  
-  /* 打开GPIO时钟 */
-	I2C_GPIO_CLK_ENABLE();
+    GPIO_InitTypeDef GPIO_InitStruct;
 
-  GPIO_InitStruct.Pin = I2C_SCL_PIN|I2C_SDA_PIN;
-  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
-  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_HIGH ;
-  HAL_GPIO_Init(I2C_GPIO_PORT, &GPIO_InitStruct);
+    /* 打开GPIO时钟 */
+    I2C_GPIO_CLK_ENABLE();
 
-	I2C_SCL(HIGH); 
-	I2C_SDA(HIGH); 
+    GPIO_InitStruct.Pin = I2C_SCL_PIN|I2C_SDA_PIN;
+    GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+    GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_HIGH ;
+    HAL_GPIO_Init(I2C_GPIO_PORT, &GPIO_InitStruct);
+
+    I2C_SCL(HIGH);
+    I2C_SDA(HIGH);
 }
 
 /**
@@ -50,14 +50,14 @@ static void I2C_InitGPIO(void)
   */
 void I2C_Start(void)
 {
-	/* 当SCL高电平时，SDA出现一个下跳沿表示I2C总线启动信号 */
-	SDA_OUT();
-	I2C_SDA(HIGH); 
-	I2C_SCL(HIGH); 
-	delay_us(4);
-	I2C_SDA(LOW); 
-	delay_us(4);
-	I2C_SCL(LOW); 
+    /* 当SCL高电平时，SDA出现一个下跳沿表示I2C总线启动信号 */
+    SDA_OUT();
+    I2C_SDA(HIGH);
+    I2C_SCL(HIGH);
+    delay_us(4);
+    I2C_SDA(LOW);
+    delay_us(4);
+    I2C_SCL(LOW);
 }
 
 /**
@@ -68,14 +68,14 @@ void I2C_Start(void)
   */
 void I2C_Stop(void)
 {
-	/* 当SCL高电平时，SDA出现一个上跳沿表示I2C总线停止信号 */
-	SDA_OUT();
-	I2C_SCL(LOW); 
-	I2C_SDA(LOW); 
-	delay_us(4);
-	I2C_SCL(HIGH); 	
-	I2C_SDA(HIGH); 
-	delay_us(4);
+    /* 当SCL高电平时，SDA出现一个上跳沿表示I2C总线停止信号 */
+    SDA_OUT();
+    I2C_SCL(LOW);
+    I2C_SDA(LOW);
+    delay_us(4);
+    I2C_SCL(HIGH);
+    I2C_SDA(HIGH);
+    delay_us(4);
 }
 
 /**
@@ -86,24 +86,24 @@ void I2C_Stop(void)
   */
 uint8_t I2C_WaitAck(void)
 {
-  uint8_t ucErrTime=0;
-	SDA_IN(); 
-	I2C_SDA(HIGH); 	/* CPU释放SDA总线 */
-	delay_us(4);
-	I2C_SCL(HIGH); 	/* CPU驱动SCL = 1, 此时器件会返回ACK应答 */
-	delay_us(4);
-	while (I2C_SDA_READ())	/* CPU读取SDA口线状态 */
-	{
-		ucErrTime++;
-		if(ucErrTime>250)
-		{
-			I2C_Stop();
-			return 1;
-		}
-	}
-	
-	I2C_SCL(LOW); 
-	return 0;
+    uint8_t ucErrTime=0;
+    SDA_IN();
+    I2C_SDA(HIGH); 	/* CPU释放SDA总线 */
+    delay_us(4);
+    I2C_SCL(HIGH); 	/* CPU驱动SCL = 1, 此时器件会返回ACK应答 */
+    delay_us(4);
+    while (I2C_SDA_READ())	/* CPU读取SDA口线状态 */
+    {
+        ucErrTime++;
+        if(ucErrTime>250)
+        {
+        	I2C_Stop();
+        	return 1;
+        }
+    }
+
+    I2C_SCL(LOW);
+    return 0;
 }
 
 /**
@@ -115,13 +115,13 @@ uint8_t I2C_WaitAck(void)
 void I2C_Ack(void)
 {
 
-	I2C_SCL(LOW); 
-	SDA_OUT(); 
+	I2C_SCL(LOW);
+	SDA_OUT();
 	I2C_SDA(LOW); 	/* CPU驱动SDA = 0 */
 	delay_us(2);
 	I2C_SCL(HIGH); 	/* CPU产生1个时钟 */
 	delay_us(2);
-	I2C_SCL(LOW); 
+	I2C_SCL(LOW);
 }
 
 /**
@@ -132,14 +132,14 @@ void I2C_Ack(void)
   */
 void I2C_NAck(void)
 {
-	
-	I2C_SCL(LOW); 
+
+	I2C_SCL(LOW);
 	SDA_OUT();
 	I2C_SDA(HIGH); 	/* CPU驱动SDA = 1 */
 	delay_us(2);
 	I2C_SCL(HIGH); 	/* CPU产生1个时钟 */
 	delay_us(2);
-	I2C_SCL(LOW); 
+	I2C_SCL(LOW);
 }
 
 /**
@@ -150,20 +150,20 @@ void I2C_NAck(void)
   */
 void I2C_SendByte(uint8_t Byte)
 {
-  uint8_t t;  
-      
-    SDA_OUT();                                                      //sda???  
-    I2C_SCL(LOW);                                                   //??????????  
-    for (t = 0; t < 8; t++)  
-    {  
-        I2C_SDA((Byte & 0x80) >> 7);  
-        Byte <<= 1;  
-        delay_us(2);  
-        I2C_SCL(HIGH);  
-        delay_us(2);  
-        I2C_SCL(LOW);  
-        delay_us(2);  
-    } 
+  uint8_t t;
+
+    SDA_OUT();
+    I2C_SCL(LOW);                                                   
+    for (t = 0; t < 8; t++)
+    {
+        I2C_SDA((Byte & 0x80) >> 7);
+        Byte <<= 1;
+        delay_us(2);
+        I2C_SCL(HIGH);
+        delay_us(2);
+        I2C_SCL(LOW);
+        delay_us(2);
+    }
 }
 
 
@@ -175,25 +175,26 @@ void I2C_SendByte(uint8_t Byte)
   */
 uint8_t I2C_ReadByte(uint8_t ack)
 {
-	uint8_t i;
-	uint8_t value;
-  SDA_IN();
-	/* 读到第1个bit为数据的bit7 */
-	value = 0;
-for (i = 0; i < 8; i++)  
-    {  
-        I2C_SCL(LOW);  
-        delay_us(2);  
-        I2C_SCL(HIGH);  
-        value <<= 1;  
-        if (I2C_SDA_READ() ) value++;  
-        delay_us(1);  
-    }  
-		if (!ack)
+    uint8_t i;
+    uint8_t value;
+    SDA_IN();
+    /* 读到第1个bit为数据的bit7 */
+    value = 0;
+    for (i = 0; i < 8; i++)
+    {
+        I2C_SCL(LOW);
+        delay_us(2);
+        I2C_SCL(HIGH);
+        value <<= 1;
+        if (I2C_SDA_READ() )
+            value++;
+        delay_us(1);
+    }
+    if (!ack)
         I2C_NAck();//发送nACK
     else
-        I2C_Ack(); //发送ACK  
-	return value;
+        I2C_Ack(); //发送ACK
+    return value;
 }
 
 
@@ -207,7 +208,7 @@ uint8_t I2C_CheckDevice(uint8_t _Address)
 {
 	uint8_t ucAck;
 
-	I2C_InitGPIO();		/* 配置GPIO */	
+	I2C_InitGPIO();		/* 配置GPIO */
 	I2C_Start();		/* 发送启动信号 */
 	/* 发送设备地址+读写控制bit（0 = w， 1 = r) bit7 先传 */
 	I2C_SendByte(_Address | I2C_WR);
@@ -230,7 +231,7 @@ void Cmd_Write_BH1750(uint8_t cmd)
 }
 void Init_BH1750(void)
 {
-	I2C_InitGPIO();	
+	I2C_InitGPIO();
 	Cmd_Write_BH1750(0x01);
 }
 void Start_BH1750(void)
@@ -239,23 +240,23 @@ void Start_BH1750(void)
 	Cmd_Write_BH1750(BH1750_CON);
 }
 void Read_BH1750(void)
-{   uint8_t i;	
+{   uint8_t i;
     I2C_Start();                          //起始信号
     I2C_SendByte(BH1750_Addr+1);         //发送设备地址+读信号
 	while(I2C_WaitAck());
-	for (i=0; i<3; i++)                      //á?D??áè?6??μ??·êy?Y￡?′?′￠?DBUF
-    {     
+	for (i=0; i<3; i++)
+    {
         if (i ==3)
         {
-        BUF[i]=I2C_ReadByte(0); 
-                   
+        BUF[i]=I2C_ReadByte(0);
+
         }
         else
-        {	BUF[i]=I2C_ReadByte(1);  //发送ACK	
-         
+        {	BUF[i]=I2C_ReadByte(1);  //发送ACK
+
        }
    }
-	
+
 	 //发送NACK
 
     I2C_Stop();                          //停止信号
@@ -267,9 +268,9 @@ float Convert_BH1750(void)
 	delay_ms(180);
 	Read_BH1750();
 	result=BUF[0];
-	result=(result<<8)+BUF[1];  //合成数据，即光照数据	
+	result=(result<<8)+BUF[1];  //合成数据，即光照数据
 	result_lx=(float)(result/1.2);
-  return result_lx;
+    return result_lx;
 }
 
 
--- targets/NB-IoT_STM32L431RxTx_IoTClub/Src/main.c
+++ targets/NB-IoT_STM32L431RxTx_IoTClub/Src/main.c
@@ -70,110 +70,110 @@ msg_for_BH1750      BH1750_send;
 
 VOID HardWare_Init(VOID)
 {
-	HAL_Init();
-	/* Configure the system clock */
-	SystemClock_Config();
+    HAL_Init();
+    /* Configure the system clock */
+    SystemClock_Config();
 
-	/* Initialize all configured peripherals */
-	DelayInit();	
-	MX_GPIO_Init();
-	MX_USART1_UART_Init();
-	MX_USART3_UART_Init();
-	MX_ADC1_Init();
-	MX_I2C1_Init();
-	printf("Welcome to IoT-Club, This is EVB-M1 Board.\r\n");
+    /* Initialize all configured peripherals */
+    DelayInit();
+    MX_GPIO_Init();
+    MX_USART1_UART_Init();
+    MX_USART3_UART_Init();
+    MX_ADC1_Init();
+    MX_I2C1_Init();
+    printf("Welcome to IoT-Club, This is EVB-M1 Board.\r\n");
 }
 
 void user_hw_init(void)
 {
-	char *str = "IoT-Club good";
-	OLED_Init();
-	DHT11_Init();	
-	OLED_Clear();
-	OLED_ShowCHinese(0,0,0);
-	OLED_ShowCHinese(18,0,1);
-	OLED_ShowCHinese(36,0,2);
-	OLED_ShowCHinese(54,0,3);
-	OLED_ShowCHinese(72,0,4);
-	OLED_ShowCHinese(90,0,5);
-	OLED_ShowString(0,2,(uint8_t*)str,16);
-	LOS_HwiCreate(EXTI0_IRQn, 2,0,EXTI0_IRQHandler,NULL);
-	LOS_HwiCreate(EXTI1_IRQn, 3,0,EXTI1_IRQHandler,NULL);
-	LOS_HwiCreate(EXTI2_IRQn, 4,0,EXTI2_IRQHandler,NULL);
-	LOS_HwiCreate(EXTI3_IRQn, 5,0,EXTI3_IRQHandler,NULL);
-	//LOS_HwiCreate(USART1_IRQn, 6,0,USART1_IRQHandler,NULL);
-	LOS_HwiCreate(USART2_IRQn, 6,0,USART2_IRQHandler,NULL);
-	LOS_HwiCreate(USART3_IRQn, 7,0,USART3_IRQHandler,NULL);
-	los_dev_uart_init(LOS_STM32L431_UART2, 9600, nbiot_uart, 500);
+    char *str = "IoT-Club good";
+    OLED_Init();
+    DHT11_Init();
+    OLED_Clear();
+    OLED_ShowCHinese(0,0,0);
+    OLED_ShowCHinese(18,0,1);
+    OLED_ShowCHinese(36,0,2);
+    OLED_ShowCHinese(54,0,3);
+    OLED_ShowCHinese(72,0,4);
+    OLED_ShowCHinese(90,0,5);
+    OLED_ShowString(0,2,(uint8_t*)str,16);
+    LOS_HwiCreate(EXTI0_IRQn, 2,0,EXTI0_IRQHandler,NULL);
+    LOS_HwiCreate(EXTI1_IRQn, 3,0,EXTI1_IRQHandler,NULL);
+    LOS_HwiCreate(EXTI2_IRQn, 4,0,EXTI2_IRQHandler,NULL);
+    LOS_HwiCreate(EXTI3_IRQn, 5,0,EXTI3_IRQHandler,NULL);
+    //LOS_HwiCreate(USART1_IRQn, 6,0,USART1_IRQHandler,NULL);
+    LOS_HwiCreate(USART2_IRQn, 6,0,USART2_IRQHandler,NULL);
+    LOS_HwiCreate(USART3_IRQn, 7,0,USART3_IRQHandler,NULL);
+    los_dev_uart_init(LOS_STM32L431_UART2, 9600, nbiot_uart, 500);
 }
 
 VOID data_collection_task(VOID)
 {
-	UINT32 uwRet = LOS_OK;
-#ifdef	MODULE_SMOKE		
-	short int Value;
-	short int MaxValue=1000;
-	SMOKE_send.index=1;
+    UINT32 uwRet = LOS_OK;
+#ifdef	MODULE_SMOKE
+    short int Value;
+    short int MaxValue=1000;
+    SMOKE_send.index=1;
 #endif
-	
-#ifdef	MODULE_GPS		
+
+#ifdef	MODULE_GPS
 
 #endif
 
 #ifdef	MODULE_BH1750
-	short int Lux;
-	Init_BH1750();
+    short int Lux;
+    Init_BH1750();
 #endif
-	user_hw_init();
+    user_hw_init();
 	while (1)
-  {
+    {
 #ifdef	MODULE_DHT11
-	  /****************temperature and humidity*****************/
-    if(DHT11_Read_TempAndHumidity(&DHT11_Data)==SUCCESS)
-    {
-			printf("读取DHT11成功!-->湿度为%.1f ％RH ，温度为 %.1f℃ \n",DHT11_Data.humidity,DHT11_Data.temperature);
-    }
-    else
-    {
-      printf("读取DHT11信息失败\n");
-			DHT11_Init();      
-    }
-		sprintf(DHT11_send.temp, "%.1f", DHT11_Data.temperature);
-		sprintf(DHT11_send.hum, "%.1f", DHT11_Data.humidity);
+          /****************temperature and humidity*****************/
+        if(DHT11_Read_TempAndHumidity(&DHT11_Data)==SUCCESS)
+        {
+            printf("读取DHT11成功!-->湿度为%.1f ％RH ，温度为 %.1f℃ \n",DHT11_Data.humidity,DHT11_Data.temperature);
+        }
+        else
+        {
+            printf("读取DHT11信息失败\n");
+            DHT11_Init();
+        }
+        sprintf(DHT11_send.temp, "%.1f", DHT11_Data.temperature);
+        sprintf(DHT11_send.hum, "%.1f", DHT11_Data.humidity);
 #endif
 
-#ifdef	MODULE_SMOKE		
-		/****************smoke******************/
-		HAL_ADC_Start(&hadc1);
-		HAL_ADC_PollForConversion(&hadc1, 50);
-		Value = HAL_ADC_GetValue(&hadc1);
-		printf("\r\n******************************MQ2 Value is  %d\r\n",Value);
-		sprintf(SMOKE_send.Value, "%4d", Value);
-		sprintf(SMOKE_send.MaxValue, "%4d", MaxValue);
+#ifdef	MODULE_SMOKE
+        /****************smoke******************/
+        HAL_ADC_Start(&hadc1);
+        HAL_ADC_PollForConversion(&hadc1, 50);
+        Value = HAL_ADC_GetValue(&hadc1);
+        printf("\r\n******************************MQ2 Value is  %d\r\n",Value);
+        sprintf(SMOKE_send.Value, "%4d", Value);
+        sprintf(SMOKE_send.MaxValue, "%4d", MaxValue);
 #endif
 
-#ifdef	MODULE_GPS		
+#ifdef	MODULE_GPS
 		/****************GPS******************/
-	  HAL_UART_Receive_IT(&huart3,gps_uart,1000);
-		NMEA_BDS_GPRMC_Analysis(&gpsmsg,(uint8_t*)gps_uart);	//分析字符串
-		Longitude=(float)((float)gpsmsg.longitude_bd/100000);	
-		printf("Longitude:%.5f %lc     \r\n",Longitude,gpsmsg.ewhemi_bd);
-		Latitude=(float)((float)gpsmsg.latitude_bd/100000);
-		printf("Latitude:%.5f %1c   \r\n",Latitude,gpsmsg.nshemi_bd);	
-  
+        HAL_UART_Receive_IT(&huart3,gps_uart,1000);
+        NMEA_BDS_GPRMC_Analysis(&gpsmsg,(uint8_t*)gps_uart);	//分析字符串
+        Longitude=(float)((float)gpsmsg.longitude_bd/100000);
+        printf("Longitude:%.5f %lc     \r\n",Longitude,gpsmsg.ewhemi_bd);
+        Latitude=(float)((float)gpsmsg.latitude_bd/100000);
+        printf("Latitude:%.5f %1c   \r\n",Latitude,gpsmsg.nshemi_bd);
+
 #endif
 
 #ifdef	MODULE_BH1750
-		/****************BH1750******************/
-		Lux=(int)Convert_BH1750();
-	  printf("\r\n******************************BH1750 Value is  %d\r\n",Lux);
-    sprintf(BH1750_send.Lux, "%5d", Lux);	
+        /****************BH1750******************/
+        Lux=(int)Convert_BH1750();
+        printf("\r\n******************************BH1750 Value is  %d\r\n",Lux);
+        sprintf(BH1750_send.Lux, "%5d", Lux);
 #endif
 
-		uwRet=LOS_TaskDelay(1500);
-		if(uwRet !=LOS_OK)
-		return;
-	
+        uwRet=LOS_TaskDelay(1500);
+        if(uwRet !=LOS_OK)
+        return;
+
   }
 }
 UINT32 creat_data_collection_task()
@@ -196,113 +196,110 @@ UINT32 creat_data_collection_task()
 
 VOID data_report_task(VOID)
 {
-	UINT32 uwRet = LOS_OK;
-	neul_bc95_reboot();                  													 //初始化模块
-	while(neul_bc95_get_netstat()<0);                              //等待连接上网络
-	//neul_bc95_set_cdpserver("180.101.147.115");                  //连接电信平台
-	neul_bc95_set_cdpserver("218.4.33.71");                        //连接华为平台
-	while(1)
-	{
-		printf("app task test\r\n");
+    UINT32 uwRet = LOS_OK;
+    neul_bc95_reboot();                                             //初始化模块
+    while(neul_bc95_get_netstat()<0);                               //等待连接上网络
+    //neul_bc95_set_cdpserver("180.101.147.115");                   //连接电信平台
+    neul_bc95_set_cdpserver("218.4.33.71");                         //连接华为平台
+    while(1)
+    {
+        printf("app task test\r\n");
 #ifdef	MODULE_DHT11
-/*******************temperature and humidity*****************/
-		if(neul_bc95_send_coap_paylaod((const char*)(&DHT11_send),sizeof(DHT11_send))>=0)		//发送数据到平台	
-				printf("ocean_send_data OK!\n");                                                //发生成功
-		else                                                                                //发送失败
-			{
-				printf("ocean_send_data Fail!\n"); 
-			}
-		uart_data_flush();
-		memset(bc95_net_data.net_nmgr, 0, 5);
-		neul_bc95_read_coap_msg(bc95_net_data.net_nmgr,5);
-		printf("%s\n",bc95_net_data.net_nmgr);
-		if(strcmp(bc95_net_data.net_nmgr,"ON")==0) //开灯
-			{	
-					HAL_GPIO_WritePin(LED_GPIO_Port,LED_Pin,GPIO_PIN_SET);    // 输出高电平
-			}
-		if(strcmp(bc95_net_data.net_nmgr,"OFF")==0) //关灯
-			{	
-					HAL_GPIO_WritePin(LED_GPIO_Port,LED_Pin,GPIO_PIN_RESET);  // 输出低电平
-			}
-/*******************************END**********************************************/
+        /*******************temperature and humidity*****************/
+        if(neul_bc95_send_coap_paylaod((const char*)(&DHT11_send),sizeof(DHT11_send))>=0)       //发送数据到平台
+            printf("ocean_send_data OK!\n");                                                    //发送成功
+        else                                                                                    //发送失败
+        {
+            printf("ocean_send_data Fail!\n");
+        }
+        uart_data_flush();
+        memset(bc95_net_data.net_nmgr, 0, 5);
+        neul_bc95_read_coap_msg(bc95_net_data.net_nmgr,5);
+        printf("%s\n",bc95_net_data.net_nmgr);
+        if(strcmp(bc95_net_data.net_nmgr,"ON")==0) //开灯
+        {
+        	HAL_GPIO_WritePin(LED_GPIO_Port,LED_Pin,GPIO_PIN_SET);    // 输出高电平
+        }
+        if(strcmp(bc95_net_data.net_nmgr,"OFF")==0) //关灯
+        {
+        	HAL_GPIO_WritePin(LED_GPIO_Port,LED_Pin,GPIO_PIN_RESET);  // 输出低电平
+        }
 #endif
-			
-#ifdef	MODULE_SMOKE	
-/***************************smoke*********************************/
-			SMOKE_send.CSQ=neul_bc95_get_csq();
-		if(neul_bc95_send_coap_paylaod((const char*)(&SMOKE_send),sizeof(SMOKE_send))>=0)		//发送数据到平台	
-			printf("ocean_send_data OK!\n");                                                  //发生成功
-		else                                                                                //发送失败
-			{
-			printf("ocean_send_data Fail!\n"); 
-			}
-		uart_data_flush();
-		memset(bc95_net_data.net_nmgr, 0, 5);
-		neul_bc95_read_coap_msg(bc95_net_data.net_nmgr,5);
-		printf("%s\n",bc95_net_data.net_nmgr);
-		if(strcmp(bc95_net_data.net_nmgr,"ON")==0) //开蜂鸣器
-			{	
-					HAL_GPIO_WritePin(Beep_GPIO_Port,Beep_Pin,GPIO_PIN_SET);    // 输出高电平
-			}
-		if(strcmp(bc95_net_data.net_nmgr,"OFF")==0) //关蜂鸣器
-			{	
-					HAL_GPIO_WritePin(Beep_GPIO_Port,Beep_Pin,GPIO_PIN_RESET);  // 输出低电平
-			}
-/*******************************END**********************************************/
+
+#ifdef	MODULE_SMOKE
+        /***************************smoke*********************************/
+        SMOKE_send.CSQ=neul_bc95_get_csq();
+        if(neul_bc95_send_coap_paylaod((const char*)(&SMOKE_send),sizeof(SMOKE_send))>=0)   //发送数据到平台
+            printf("ocean_send_data OK!\n");                                                //发送成功
+        else                                                                                //发送失败
+        {
+            printf("ocean_send_data Fail!\n");
+        }
+        uart_data_flush();
+        memset(bc95_net_data.net_nmgr, 0, 5);
+        neul_bc95_read_coap_msg(bc95_net_data.net_nmgr,5);
+        printf("%s\n",bc95_net_data.net_nmgr);
+        if(strcmp(bc95_net_data.net_nmgr,"ON")==0) //开蜂鸣器
+        {
+            HAL_GPIO_WritePin(Beep_GPIO_Port,Beep_Pin,GPIO_PIN_SET);    // 输出高电平
+        }
+        if(strcmp(bc95_net_data.net_nmgr,"OFF")==0) //关蜂鸣器
+        {
+            HAL_GPIO_WritePin(Beep_GPIO_Port,Beep_Pin,GPIO_PIN_RESET);  // 输出低电平
+        }
 #endif
-			
+
 #ifdef	MODULE_GPS
-/***********************************GPS*************************************/
-		  if(Latitude!=0&&Longitude!=0)
-		{
-			memset(GPS_send.Latitude, 0, 8);
-			memset(GPS_send.Longitude, 0, 9);
-			sprintf(GPS_send.Latitude, "%.5f", Latitude);
-			sprintf(GPS_send.Longitude, "%.5f", Longitude);		
-			if(neul_bc95_send_coap_paylaod((const char*)(&GPS_send),sizeof(GPS_send))>=0)		//发送数据到平台	
-				printf("ocean_send_data OK!\n");                                            //发生成功
-		else                                                                            //发送失败
-			{
-				printf("ocean_send_data Fail!\n"); 
-			}
-			uart_data_flush();
-		memset(bc95_net_data.net_nmgr, 0, 5);
-		neul_bc95_read_coap_msg(bc95_net_data.net_nmgr,5);
-		printf("%s\n",bc95_net_data.net_nmgr);
-		if(strcmp(bc95_net_data.net_nmgr,"ON")==0) //开灯
-			{	
-					HAL_GPIO_WritePin(LED_GPIO_Port,LED_Pin,GPIO_PIN_SET);    // 输出高电平
-			}
-		if(strcmp(bc95_net_data.net_nmgr,"OFF")==0) //关灯
-			{	
-					HAL_GPIO_WritePin(LED_GPIO_Port,LED_Pin,GPIO_PIN_RESET);  // 输出低电平
-			}
-		}
-/*******************************END**********************************************/
+        /***********************************GPS*************************************/
+        if(Latitude!=0&&Longitude!=0)
+        {
+            memset(GPS_send.Latitude, 0, 8);
+            memset(GPS_send.Longitude, 0, 9);
+            sprintf(GPS_send.Latitude, "%.5f", Latitude);
+            sprintf(GPS_send.Longitude, "%.5f", Longitude);
+            if(neul_bc95_send_coap_paylaod((const char*)(&GPS_send),sizeof(GPS_send))>=0)   //发送数据到平台
+                printf("ocean_send_data OK!\n");                                            //发送成功
+            else                                                                            //发送失败
+            {
+                printf("ocean_send_data Fail!\n");
+            }
+            uart_data_flush();
+            memset(bc95_net_data.net_nmgr, 0, 5);
+            neul_bc95_read_coap_msg(bc95_net_data.net_nmgr,5);
+            printf("%s\n",bc95_net_data.net_nmgr);
+            if(strcmp(bc95_net_data.net_nmgr,"ON")==0) //开灯
+            {
+                HAL_GPIO_WritePin(LED_GPIO_Port,LED_Pin,GPIO_PIN_SET);    // 输出高电平
+            }
+            if(strcmp(bc95_net_data.net_nmgr,"OFF")==0) //关灯
+            {
+                HAL_GPIO_WritePin(LED_GPIO_Port,LED_Pin,GPIO_PIN_RESET);  // 输出低电平
+            }
+        }
+
 #endif
-			
+
 #ifdef	MODULE_BH1750
-/**********************************BH1750*************************************/
-		if(neul_bc95_send_coap_paylaod((const char*)(&BH1750_send),sizeof(BH1750_send))>=0)		//发送数据到平台	
-				printf("ocean_send_data OK!\n");                                                  //发生成功
-		else                                                                                  //发送失败
-			{
-				printf("ocean_send_data Fail!\n"); 
-			}
-			uart_data_flush();
-		memset(bc95_net_data.net_nmgr, 0, 5);
-		neul_bc95_read_coap_msg(bc95_net_data.net_nmgr,5);
-		printf("%s\n",bc95_net_data.net_nmgr);
-		if(strcmp(bc95_net_data.net_nmgr,"ON")==0) //开灯
-			{	
-					HAL_GPIO_WritePin(Light_GPIO_Port,Light_Pin,GPIO_PIN_RESET);    // 输出低电平
-			}
-		if(strcmp(bc95_net_data.net_nmgr,"OFF")==0) //关灯
-			{	
-					HAL_GPIO_WritePin(Light_GPIO_Port,Light_Pin,GPIO_PIN_SET);  // 输出高电平
-			}
-/*******************************END**********************************************/
-#endif 
+        /**********************************BH1750*************************************/
+        if(neul_bc95_send_coap_paylaod((const char*)(&BH1750_send),sizeof(BH1750_send))>=0)         //发送数据到平台
+            printf("ocean_send_data OK!\n");                                                        //发送成功
+        else                                                                                        //发送失败
+        {
+            printf("ocean_send_data Fail!\n");
+        }
+        uart_data_flush();
+        memset(bc95_net_data.net_nmgr, 0, 5);
+        neul_bc95_read_coap_msg(bc95_net_data.net_nmgr,5);
+        printf("%s\n",bc95_net_data.net_nmgr);
+        if(strcmp(bc95_net_data.net_nmgr,"ON")==0) //开灯
+        {
+            HAL_GPIO_WritePin(Light_GPIO_Port,Light_Pin,GPIO_PIN_RESET);    // 输出低电平
+        }
+        if(strcmp(bc95_net_data.net_nmgr,"OFF")==0) //关灯
+        {
+            HAL_GPIO_WritePin(Light_GPIO_Port,Light_Pin,GPIO_PIN_SET);  // 输出高电平
+        }
+#endif
 	  uwRet=LOS_TaskDelay(500);
 		if(uwRet !=LOS_OK)
 		return;
@@ -328,7 +325,7 @@ UINT32 creat_data_report_task()
 int main(void)
 {
     UINT32 uwRet = LOS_OK;
-	
+
     HardWare_Init();
 
     uwRet = LOS_KernelInit();
@@ -336,14 +333,14 @@ int main(void)
     {
         return LOS_NOK;
     }
-		
+
     uwRet = creat_data_collection_task();
     if (uwRet != LOS_OK)
     {
         return LOS_NOK;
     }
-		
-		uwRet = creat_data_report_task();
+
+    uwRet = creat_data_report_task();
     if (uwRet != LOS_OK)
     {
         return LOS_NOK;
