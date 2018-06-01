5bc28d0dbbb525961f923833f4d859c1645fcbc4
l00438842 <lumin17@huawei.com>
2018-06-01 14:57:39 +0800

    add paho.mqtt.embedded-c-1.1.0 as the MQTT client source code

---
+++ components/connectivity/paho.mqtt.embedded-c-1.1.0/Debug/sources.mk
@@ -0,0 +1,18 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+ASM_SRCS := 
+OBJ_SRCS := 
+S_UPPER_SRCS := 
+C_SRCS := 
+O_SRCS := 
+C_DEPS := 
+LIBRARIES := 
+OBJS := 
+
+# Every subdirectory with source files must be described here
+SUBDIRS := \
+MQTTPacket/src \
+MQTTPacket/samples \
+
