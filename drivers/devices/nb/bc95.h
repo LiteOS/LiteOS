54760b679b5d867d3d38f05af5904a2dbf8f756c
songjiannan <song_jiannan@hoperun.com>
2018-06-11 20:19:57 +0800

    1. add bc95 source files to makefile 2. add macro in bc95.h

--- drivers/devices/nb/bc95.h
+++ drivers/devices/nb/bc95.h
@@ -3,13 +3,24 @@
 
 #include "at_api_interface.h"
 
-#define CLOUD_IP  "218.4.33.71,5683"
-#define AT_NB_LINE_END 			"\r\n"
+#define CLOUD_IP             "218.4.33.71,5683"
+#define AT_NB_LINE_END       "\r\n"
 
-#define AT_NB_reboot    		"AT+NRB\r"
-#define AT_NB_hw_detect    		"AT+CFUN?\r"
-#define AT_NB_get_auto_connect    		"AT+NCONFIG?\r"
-#define AT_CMD_PREFIX      "+NNMI:"
+#define AT_NB_reboot         "AT+NRB\r"
+#define AT_NB_hw_detect      "AT+CFUN?\r"
+#define AT_NB_get_auto_connect            "AT+NCONFIG?\r"
+#define AT_CMD_PREFIX          "+NNMI:"
+
+#define AT_MODU_NAME        "nb_neul95"
+#define AT_USART            USART3
+#define AT_BUARDRATE        9600
+#define AT_USART_IRQn       USART3_IRQn
+#define AT_CMD_TIMEOUT      10000    //ms
+#define AT_MAX_LINK_NUM     4
+
+#define AT_LINE_END         "\r\n"
+#define AT_CMD_BEGIN        "\r\n"
+#define MAX_AT_USERDATA_LEN 2048
 
 typedef struct _remote_info_t
 {
