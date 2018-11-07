e3450ba81a9eb1b2dd94bf0019453e5fb9d6b183
Zhan <zhanjin2009@foxmail.com>
2018-11-07 14:45:39 +0800

    优化sota

--- targets/Cloud_STM32F429IGTx_FIRE/Src/main.c
+++ targets/Cloud_STM32F429IGTx_FIRE/Src/main.c
@@ -117,16 +117,19 @@ int32_t sota_callback(void *arg, int8_t* buf, int32_t buflen)
 
 void nb_sota_demo()
 {
-    sota_op_t flash_op =
+    sota_opt_t flash_op =
     {
     .get_ver = read_ver,
     .set_ver = set_ver,
     .sota_send = nb_send_str,
+    .sota_malloc = at_malloc,
+    .sota_free = at_free,
     };
     hal_get_ota_opt(&flash_op.ota_info);
     flash_op.ota_info.key.rsa_N = "C94BECB7BCBFF459B9A71F12C3CC0603B11F0D3A366A226FD3E73D453F96EFBBCD4DFED6D9F77FD78C3AB1805E1BD3858131ACB5303F61AF524F43971B4D429CB847905E68935C1748D0096C1A09DD539CE74857F9FDF0B0EA61574C5D76BD9A67681AC6A9DB1BB22F17120B1DBF3E32633DCE34F5446F52DD7335671AC3A1F21DC557FA4CE9A4E0E3E99FED33A0BAA1C6F6EE53EDD742284D6582B51E4BF019787B8C33C2F2A095BEED11D6FE68611BD00825AF97DB985C62C3AE0DC69BD7D0118E6D620B52AFD514AD5BFA8BAB998332213D7DBF5C98DC86CB8D4F98A416802B892B8D6BEE5D55B7E688334B281E4BEDDB11BD7B374355C5919BA5A9A1C91F";
     flash_op.ota_info.key.rsa_E = "10001";
-    flash_op.user_data_len = 1224;
+    flash_op.frame_buf_len = 1224;
+    flash_op.run_mode = APP_MODE;
     hal_init_ota();
 
     sota_init(&flash_op);
