434c77b4d9e55e01b724d11b55b7f021c7c8bac9
zgbset <zgbset@163.com>
2018-11-09 17:24:25 +0800

    fix: support thread safety of errno

--- include/fs/sys/errno.h
+++ include/fs/sys/errno.h
@@ -1,17 +1,46 @@
-#ifndef _SYS_ERRNO_H_
-#define _SYS_ERRNO_H_
+/*----------------------------------------------------------------------------
+ * Copyright (c) <2013-2018>, <Huawei Technologies Co., Ltd>
+ * All rights reserved.
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted provided that the following conditions are met:
+ * 1. Redistributions of source code must retain the above copyright notice, this list of
+ * conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice, this list
+ * of conditions and the following disclaimer in the documentation and/or other materials
+ * provided with the distribution.
+ * 3. Neither the name of the copyright holder nor the names of its contributors may be used
+ * to endorse or promote products derived from this software without specific prior written
+ * permission.
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+ * OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+ * OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *---------------------------------------------------------------------------*/
+/*----------------------------------------------------------------------------
+ * Notice of Export Control Law
+ * ===============================================
+ * Huawei LiteOS may be subject to applicable export control laws and regulations, which might
+ * include those applicable to Huawei LiteOS of U.S. and the country in which you are located.
+ * Import, export and usage of Huawei LiteOS in any manner by you shall be in compliance with such
+ * applicable export control laws and regulations.
+ *---------------------------------------------------------------------------*/
+
+#ifndef _ERRNO_H
+#define _ERRNO_H
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-
-#ifdef __CC_ARM
-#define _ARMABI_PURE __declspec(__nothrow) __attribute__((const))
-extern _ARMABI_PURE volatile int *__aeabi_errno_addr(void);
-#define errno (*__CLIBNS __aeabi_errno_addr())
-#endif
-
+extern int *task_errno(void);
+#define errno (*task_errno())
 
 #define	EPERM 1		/* Not owner */
 #define	ENOENT 2	/* No such file or directory */
@@ -174,4 +203,4 @@ extern _ARMABI_PURE volatile int *__aeabi_errno_addr(void);
 #ifdef __cplusplus
 }
 #endif
-#endif /* _SYS_ERRNO_H */
+#endif /* _ERRNO_H */
