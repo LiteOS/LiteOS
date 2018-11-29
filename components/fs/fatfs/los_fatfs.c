54aae7b525aa2d4e20f8c0fa08d5676269cf929f
hong_liming <hong_liming@hoperun.com>
2018-11-29 18:37:13 +0800

    optimize fs (error,remove ioctrl,open)

--- components/fs/fatfs/los_fatfs.c
+++ components/fs/fatfs/los_fatfs.c
@@ -356,7 +356,13 @@ static off_t fatfs_op_lseek (struct file *file, off_t off, int whence)
 
     FRESULT res = f_lseek(fp, off);
     if (res == FR_OK)
-        return off;
+    {
+    	if(off < 0)
+    	{
+    		ret_to_errno(FR_INVALID_PARAMETER);
+    	}
+    	return off;
+    }
     else
         return ret_to_errno(res);
 }
