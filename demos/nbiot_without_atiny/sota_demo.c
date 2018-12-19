33fd58950db22de60443f5689180d6ebb8c6a589
w00164089 <37469733+w00164089@users.noreply.github.com>
2018-12-19 14:27:32 +0800

    Support sota restart downloading (#670)

--- demos/nbiot_without_atiny/sota_demo.c
+++ demos/nbiot_without_atiny/sota_demo.c
@@ -34,6 +34,7 @@
 
 #ifdef WITH_SOTA
 #include <stdio.h>
+#include "los_swtmr.h"
 #include "osdepends/atiny_osdep.h"
 #include "sota/sota.h"
 #include "../../components/ota/sota/sota_hal.h"
@@ -42,8 +43,13 @@
 #include "hal_spi_flash.h"
 #include "at_frame/at_main.h"
 
+#define DEVICE_VER             "V0.0"
+#define SOTA_BUF_LEN           (512+16)
+#define SOTA_MAX_TIME_OUT       5000
 extern int nb_send_str(const char* buf, int len);
-#define DEVICE_VER "V0.0"
+
+int8_t g_sota_buf[SOTA_BUF_LEN];
+uint16_t g_sota_timer;
 int read_ver(char* buf, uint32_t len)
 {
     memcpy(buf,DEVICE_VER,strlen(DEVICE_VER));
@@ -54,6 +60,7 @@ int notify_new_ver(char* buf, uint32_t len)
 {
     return 0;
 }
+
 int set_ver(const char* buf, uint32_t len)
 {
     return 0;
@@ -61,20 +68,45 @@ int set_ver(const char* buf, uint32_t len)
 
 int32_t sota_cmd_match(const char *buf, char* featurestr,int len)
 {
-    
     if(strstr(buf,featurestr) != NULL)
-        return 0;
-    else
-        return -1;
+    {
+        if (sota_parse((const int8_t *)buf, len, g_sota_buf, SOTA_BUF_LEN) == SOTA_OK)
+        {
+            return 0;
+        }
+    }
+
+    return -1;
 }
 
 int32_t sota_callback(void *arg, int8_t* buf, int32_t buflen)
 {
     int ret;
-    ret = sota_process_main(arg, buf, buflen);
-    if (ret == SOTA_NEEDREBOOT)
+    ret = sota_process(arg, g_sota_buf, buflen);
+
+    switch (ret)
     {
-        atiny_reboot();
+        case SOTA_UPDATED:
+        {
+            LOS_SwtmrStop(g_sota_timer);
+            atiny_reboot();
+            break;
+        }
+        case SOTA_DOWNLOADING:
+        case SOTA_UPDATING:
+        {
+            LOS_SwtmrStart(g_sota_timer);
+            break;
+        }
+        case SOTA_EXIT:
+        case SOTA_WRITE_FLASH_FAILED:
+        {
+            LOS_SwtmrStop(g_sota_timer);
+            break;
+        }
+        default:
+        {
+        }          
     }
     return 0;
 }
@@ -100,22 +132,19 @@ void nb_sota_demo(void)
 {
     sota_opt_t flash_op =
     {
-    .get_ver = read_ver,
-    .set_ver = set_ver,
-    .sota_send = nb_send_str,
-    .sota_malloc = at_malloc,
-    .sota_printf = sota_log,
-    .sota_free = at_free,
+       .get_ver = read_ver,
+       .sota_send = nb_send_str,
+       .sota_malloc = at_malloc,
+       .sota_printf = sota_log,
+       .sota_free = at_free,
     };
     hal_get_ota_opt(&flash_op.ota_info);
     flash_op.ota_info.key.rsa_N = "C94BECB7BCBFF459B9A71F12C3CC0603B11F0D3A366A226FD3E73D453F96EFBBCD4DFED6D9F77FD78C3AB1805E1BD3858131ACB5303F61AF524F43971B4D429CB847905E68935C1748D0096C1A09DD539CE74857F9FDF0B0EA61574C5D76BD9A67681AC6A9DB1BB22F17120B1DBF3E32633DCE34F5446F52DD7335671AC3A1F21DC557FA4CE9A4E0E3E99FED33A0BAA1C6F6EE53EDD742284D6582B51E4BF019787B8C33C2F2A095BEED11D6FE68611BD00825AF97DB985C62C3AE0DC69BD7D0118E6D620B52AFD514AD5BFA8BAB998332213D7DBF5C98DC86CB8D4F98A416802B892B8D6BEE5D55B7E688334B281E4BEDDB11BD7B374355C5919BA5A9A1C91F";
     flash_op.ota_info.key.rsa_E = "10001";
-    flash_op.frame_buf_len = 1224;
-    flash_op.run_mode = APP_MODE;
     hal_init_ota();
-
+    LOS_SwtmrCreate(SOTA_MAX_TIME_OUT, LOS_SWTMR_MODE_NO_SELFDELETE, (SWTMR_PROC_FUNC)sota_timeout_handler, &g_sota_timer, 1, OS_SWTMR_ROUSES_ALLOW, OS_SWTMR_ALIGN_SENSITIVE);
     sota_init(&flash_op);
-    (void)at.oob_register("+NNMI:", strlen("+NNMI:"), sota_callback,sota_cmd_match);
+    (void)at.oob_register("+NNMI:", strlen("+NNMI:"), sota_callback, sota_cmd_match);
 }
 
 #endif
